46baabb022b1a4fd9277969ee294c210cae96c8a
Binh Nguyen <binh.q.nguyen527@gmail.com>
2020-10-19 09:43:59 +0700

    Complete skeleton mock UI for ResultFragment

--- app/src/main/java/com/github/bnguyen527/snaptuney/ResultFragment.kt
+++ app/src/main/java/com/github/bnguyen527/snaptuney/ResultFragment.kt
@@ -4,13 +4,38 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.github.bnguyen527.snaptuney.databinding.FragmentResultBinding
 
 class ResultFragment : Fragment() {
+    private var _binding: FragmentResultBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_result, container, false)
+        _binding = FragmentResultBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            savePlaylistButton.setOnClickListener {
+                Toast.makeText(context, "Saved to Spotify!", Toast.LENGTH_SHORT).show()
+            }
+            newConfigurationsButton.setOnClickListener {
+                findNavController().navigate(ResultFragmentDirections.actionNewConfigurations())
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
