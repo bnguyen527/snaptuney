aca6448e7bf40d5d3c10ee3c23a57cdf45d4ac86
Binh Nguyen <binh.q.nguyen527@gmail.com>
2020-10-19 09:27:12 +0700

    Complete skeleton mock UI for ConfigurationsFragment

--- app/src/main/java/com/github/bnguyen527/snaptuney/ConfigurationsFragment.kt
+++ app/src/main/java/com/github/bnguyen527/snaptuney/ConfigurationsFragment.kt
@@ -5,12 +5,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.github.bnguyen527.snaptuney.databinding.FragmentConfigurationsBinding
 
 class ConfigurationsFragment : Fragment() {
+    private var _binding: FragmentConfigurationsBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_configurations, container, false)
+        _binding = FragmentConfigurationsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            createPlaylistButton.setOnClickListener {
+                findNavController().navigate(ConfigurationsFragmentDirections.actionCreatePlaylist())
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
