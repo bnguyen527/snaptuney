a2aea755b5e63139529316bbde3038eae1318e68
Binh Nguyen <binh.q.nguyen527@gmail.com>
2020-10-19 21:01:46 +0700

    Add basic login functionality
    Able to log in on button tap and show responsive toast message.

--- app/src/main/java/com/github/bnguyen527/snaptuney/WelcomeActivity.kt
+++ app/src/main/java/com/github/bnguyen527/snaptuney/WelcomeActivity.kt
@@ -3,8 +3,12 @@ package com.github.bnguyen527.snaptuney
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.github.bnguyen527.snaptuney.databinding.ActivityWelcomeBinding
+import com.spotify.sdk.android.auth.AuthorizationClient
+import com.spotify.sdk.android.auth.AuthorizationRequest
+import com.spotify.sdk.android.auth.AuthorizationResponse
 
 class WelcomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityWelcomeBinding
@@ -16,13 +20,54 @@ class WelcomeActivity : AppCompatActivity() {
 
         binding.apply {
             loginButton.setOnClickListener {
-                loginButton.visibility = View.GONE
-                loggedInTextView.visibility = View.VISIBLE
-                startMakingButton.visibility = View.VISIBLE
+                val request = AuthorizationRequest.Builder(
+                    CLIENT_ID, AuthorizationResponse.Type.TOKEN, REDIRECT_URI
+                ).build()
+                AuthorizationClient.openLoginActivity(this@WelcomeActivity, REQUEST_CODE, request)
             }
             startMakingButton.setOnClickListener {
                 startActivity(Intent(this@WelcomeActivity, MainActivity::class.java))
             }
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_CODE) {
+            val response = AuthorizationClient.getResponse(resultCode, data)
+            when (response.type) {
+                AuthorizationResponse.Type.TOKEN -> {
+                    binding.apply {
+                        loginButton.visibility = View.GONE
+                        loggedInTextView.visibility = View.VISIBLE
+                        startMakingButton.visibility = View.VISIBLE
+                    }
+                    Toast.makeText(
+                        this,
+                        getString(R.string.login_success_toast_text),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                AuthorizationResponse.Type.ERROR ->
+                    Toast.makeText(
+                        this,
+                        getString(R.string.login_failed_toast_text),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                else ->
+                    Toast.makeText(
+                        this,
+                        getString(R.string.login_canceled_toast_text),
+                        Toast.LENGTH_SHORT
+                    ).show()
+            }
+        }
+    }
+
+    companion object {
+        private const val CLIENT_ID = "cceb1c861c3a4cbfb6de3c67dfc32179"
+        private const val REDIRECT_URI = "com.github.bnguyen527.snaptuney://callback"
+        private const val REQUEST_CODE = 1337
     }
 }
