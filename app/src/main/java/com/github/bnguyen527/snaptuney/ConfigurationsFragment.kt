8dea2169b8f8f6fbc9e1def98fbc512d3a093ba0
Binh Nguyen <binh.q.nguyen527@gmail.com>
2020-10-21 10:55:07 +0700

    Fetch user's and featured playlists

--- app/src/main/java/com/github/bnguyen527/snaptuney/ConfigurationsFragment.kt
+++ app/src/main/java/com/github/bnguyen527/snaptuney/ConfigurationsFragment.kt
@@ -1,13 +1,20 @@
 package com.github.bnguyen527.snaptuney
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.github.bnguyen527.snaptuney.databinding.FragmentConfigurationsBinding
+import kaaes.spotify.webapi.android.SpotifyError
 import kaaes.spotify.webapi.android.SpotifyService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit.RetrofitError
 
 class ConfigurationsFragment : Fragment() {
     private var _binding: FragmentConfigurationsBinding? = null
@@ -37,6 +44,27 @@ class ConfigurationsFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         _spotify = (requireActivity() as MainActivity).spotify
+
+        lifecycleScope.launch {
+            withContext(Dispatchers.IO) {
+                try {
+                    val myPlaylists = spotify.myPlaylists
+                    Log.d(TAG, "Fetched ${myPlaylists.total} playlists")
+                    Log.i(TAG, "Fetched list of current user's playlists")
+                } catch (error: RetrofitError) {
+                    Log.e(TAG, SpotifyError.fromRetrofitError(error).toString())
+                }
+            }
+            withContext(Dispatchers.IO) {
+                try {
+                    val featuredPlaylists = spotify.featuredPlaylists.playlists
+                    Log.d(TAG, "Fetched ${featuredPlaylists.total} playlists")
+                    Log.i(TAG, "Fetched list of featured playlists")
+                } catch (error: RetrofitError) {
+                    Log.e(TAG, SpotifyError.fromRetrofitError(error).toString())
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
@@ -47,5 +75,9 @@ class ConfigurationsFragment : Fragment() {
     override fun onDetach() {
         super.onDetach()
         _spotify = null
+    }
+
+    companion object {
+        private val TAG = ConfigurationsFragment::class.java.simpleName
     }
 }
