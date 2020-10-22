ed699f3d4515bc98fa2c1527c79539b57007cfb4
Binh Nguyen <binh.q.nguyen527@gmail.com>
2020-10-22 10:28:35 +0700

    Fetch source playlists from input configurations

--- app/src/main/java/com/github/bnguyen527/snaptuney/ResultFragment.kt
+++ app/src/main/java/com/github/bnguyen527/snaptuney/ResultFragment.kt
@@ -7,10 +7,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.github.bnguyen527.snaptuney.databinding.FragmentResultBinding
+import kaaes.spotify.webapi.android.SpotifyError
 import kaaes.spotify.webapi.android.SpotifyService
+import kaaes.spotify.webapi.android.models.PlaylistTrack
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit.RetrofitError
 
 class ResultFragment : Fragment() {
     private var _binding: FragmentResultBinding? = null
@@ -47,7 +54,31 @@ class ResultFragment : Fragment() {
         val configurations by navArgs<ResultFragmentArgs>()
         Log.d(TAG, configurations.toString())
         Log.i(TAG, "Received input configurations")
+        lifecycleScope.launch {
+            with(configurations) {
+                fetchPlaylistTracks(firstSourceOwnerId, firstSourceId)
+                fetchPlaylistTracks(secondSourceOwnerId, secondSourceId)
+            }
+        }
     }
+
+    /**
+     * Returns fetched playlist tracks of playlist [playlistId] owned by user [ownerId] and empty
+     * list when API request response is an error.
+     */
+    private suspend fun fetchPlaylistTracks(ownerId: String, playlistId: String) =
+        withContext(Dispatchers.IO) {
+            try {
+                val playlistTracksPager =
+                    spotify.getPlaylistTracks(ownerId, playlistId, PLAYLIST_TRACKS_QUERY_OPTIONS)
+                Log.d(TAG, "Fetched ${playlistTracksPager.items.size} tracks")
+                Log.i(TAG, "Fetched tracks from $playlistId")
+                playlistTracksPager.items
+            } catch (error: RetrofitError) {
+                Log.e(TAG, SpotifyError.fromRetrofitError(error).toString())
+                emptyList<PlaylistTrack>()
+            }
+        }
 
     override fun onDestroyView() {
         super.onDestroyView()
@@ -61,5 +92,9 @@ class ResultFragment : Fragment() {
 
     companion object {
         private val TAG = ResultFragment::class.java.simpleName
+        private val PLAYLIST_TRACKS_QUERY_OPTIONS = mapOf(
+            "fields" to "items.track(artists.name,duration_ms,id,name,uri)",
+            "market" to "from_token"
+        )
     }
 }
